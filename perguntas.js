ac177043b17932e52cc12645c05de90499ca9e32
cotozin <00001121483781sp@al.educacao.sp.gov.br>
2024-10-29 09:17:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Matemática',
+    'qual a raiz quadrada de 9',
+    ' A raiz quadrada de 9 é igual a 3, pois, 3² é 9'
+)
+
+criaCartao(
+    'Técnologia e Robótica',
+    'Qual os comando que copia e cola uma palavra no VScode ',
+    ' Ctrl + C copia e Ctrl + v cola'
+    
+)
+
+criaCartao(
+    'Empreendedorismo',
+    'quais os objetivos do empreendedorismo',
+    ' criar um valor, gerar um impacto positivo'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'como se diz (eu vou para casa em ingles)',
+    ' Im going home'
+)
